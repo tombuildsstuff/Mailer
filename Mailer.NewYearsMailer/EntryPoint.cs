9d1f106e8b840a1eca4f253b576aaacbc012c6c2
Tom Harvey <tom@ibuildstuff.co.uk>
2011-12-24 16:13:13 +0000

    Started adding in the functionality to load contacts from Google Contacts

--- Mailer.NewYearsMailer/EntryPoint.cs
+++ Mailer.NewYearsMailer/EntryPoint.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using Mailer.Repositories.Google;
 using Mailer.Repositories.LocalFileSystem;
 
 namespace Mailer.NewYearsMailer
@@ -7,8 +9,11 @@ namespace Mailer.NewYearsMailer
     {
         public static void Main(string[] args)
         {
-            var repository = new LocalFileSystemContactsRepository("C:\\Development\\Contacts.csv");
-            var contacts = repository.GetAll();
+            //var repository = new LocalFileSystemContactsRepository("C:\\Development\\Contacts.csv");
+            var contactsRepository = new GoogleContactsRepository("New Years Mailer", "tom@XXXXXX.co.uk", "XXXXXX");
+            var groupsRepository = new GoogleGroupsRepository("New Years Mailer", "tom@XXXXXX.co.uk", "XXXXXX");
+            var groupId = groupsRepository.GetAll().Where(g => g.Name == "Friends").FirstOrDefault().Id;
+            var contacts = contactsRepository.GetAllInGroup(groupId);
             foreach (var contact in contacts)
             {
                 Console.WriteLine(contact.FullName);
--- Mailer.Repositories/IContactsRepository.cs
+++ Mailer.Repositories/IContactsRepository.cs
@@ -5,6 +5,10 @@ namespace Mailer.Repositories
 {
     public interface IContactsRepository
     {
+        IEnumerable<Contact> GetAllInGroup(string groupId);
+
+        IEnumerable<Contact> GetAllInGroups(IEnumerable<string> groupIds);
+        
         IEnumerable<Contact> GetAll();
     }
 }
