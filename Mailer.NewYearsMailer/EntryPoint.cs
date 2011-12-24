15a6702b1b1d32d222f3ad7e14b230e2ae1c4f37
Tom Harvey <tom@ibuildstuff.co.uk>
2011-12-24 17:53:50 +0000

    Added in loading of the settings for the app from the App.Config

--- Mailer.NewYearsMailer/EntryPoint.cs
+++ Mailer.NewYearsMailer/EntryPoint.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using Mailer.Repositories.Google;
-using Mailer.Repositories.LocalFileSystem;
 
 namespace Mailer.NewYearsMailer
 {
@@ -9,8 +8,9 @@ namespace Mailer.NewYearsMailer
     {
         public static void Main(string[] args)
         {
+            var googleCredentials = new GoogleContactsConfiguration();
             //var repository = new LocalFileSystemContactsRepository("C:\\Development\\Contacts.csv");
-            var credentials = new GoogleAccountCredentials("New Years Mailer", "tom@XXXXXX.co.uk", "XXXXXX");
+            var credentials = new GoogleAccountCredentials("New Years Mailer", googleCredentials.GoogleUsername, googleCredentials.GooglePassword);
             var contactsRepository = new GoogleContactsRepository(credentials);
             var groupsRepository = new GoogleGroupsRepository(credentials);
             var groupId = groupsRepository.GetAll().Where(g => g.Name == "Friends").FirstOrDefault().Id;
---
+++ Mailer.Repositories.Google/GoogleContactsConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+namespace Mailer.Repositories.Google
+{
+    public class GoogleContactsConfiguration
+    {
+        public string GoogleUsername { get { return ConfigurationManager.AppSettings["GoogleUsername"]; } }
+
+        public string GooglePassword { get { return ConfigurationManager.AppSettings["GooglePassword"]; } }
+
+        public string MaleTextMessage { get { return ConfigurationManager.AppSettings["MaleTextMessage"]; } }
+
+        public string FemaleTextMessage { get { return ConfigurationManager.AppSettings["FemaleTextMessage"]; } }
+
+        public IEnumerable<string> GroupsToMessage { get { return ConfigurationManager.AppSettings["GroupsToMessage"].Split(','); } }
+    }
+}
